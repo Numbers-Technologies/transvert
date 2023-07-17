f1309ae62c0188e9e18710757b60410c8f4a1e17
Daniil Ermolaev <blcklptn@icloud.com>
2023-07-17 16:56:02 +0500

    dotenv

--- bot/src/core/config.py
+++ bot/src/core/config.py
@@ -1,7 +1,10 @@
 # All settings data from .env will be in Settings class.
 # Written by Daniil Ermolaev <blcklptn@icloud.com> 17.07.2023
 from pydantic import BaseSettings
+from dotenv import load_dotenv
 
+
+load_dotenv()
 
 class Settings(BaseSettings):
     BOT_TOKEN: str
