ff68aac23c608729e908639ece746789e386b330
Daniil Ermolaev <blcklptn@icloud.com>
2023-07-17 17:31:16 +0500

    state 1.1 is done

--- bot/src/core/config.py
+++ bot/src/core/config.py
@@ -1,5 +1,6 @@
 # All settings data from .env will be in Settings class.
 # Written by Daniil Ermolaev <blcklptn@icloud.com> 17.07.2023
+
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 
