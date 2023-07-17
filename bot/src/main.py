3fcfeb91b5436ca9fc3b17b394da5537c1e309b7
Daniil Ermolaev <blcklptn@icloud.com>
2023-07-17 16:59:06 +0500

    main function

---
+++ bot/src/main.py
@@ -0,0 +1,7 @@
+# Main function that run all bot.
+# Written by Daniil Ermolaev <blcklptn@icloud.com> 17.07.2023.
+from dispatcher import *
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp) # db ~ custom dispatcher implementation
